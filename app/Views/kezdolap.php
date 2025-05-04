ad3883c5c2e3b424b758465ca6b935fcadb1fe52
barazd <barazd@gmail.com>
2025-05-04 23:46:25 +0200

    add: video háttér a kezdőlapra

--- app/Views/kezdolap.php
+++ app/Views/kezdolap.php
@@ -1,6 +1,15 @@
 <?php include 'Partials/layout.header.php' ?>
 
     <div class="content">
+        <div class="hero">
+            <video playsinline autoplay loop poster="/assets/webprog1_hero.png">
+                <source src="/assets/webprog1_hero.mp4" type="video/mp4">
+            </video>
+            <div class="content">
+                <h2>Vasút</h2>
+                <p> &#11025; az oldal témája</p>
+            </div>
+        </div>
         <div class="box">
             <h2>Kezdőlap</h2>
             <p>Ide jön majd a tartalom...</p>
@@ -8,6 +17,7 @@
         </div>
     </div>
     <aside>
+        
         <div class="box">
             <h2>Oldalsáv</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, amet optio deleniti aliquid laboriosam quas beatae quod quam libero repudiandae, dolor nihil quaerat, totam alias saepe numquam quidem? Voluptates, nostrum.</p>
