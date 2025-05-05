8c7866ae4f89168e32b1f43b366865e0cdece7ca
barazd <barazd@gmail.com>
2025-05-05 23:28:50 +0200

    add: JS validáció

--- app/Views/kapcsolat.php
+++ app/Views/kapcsolat.php
@@ -20,28 +20,38 @@
         <div class="box">
             <h2>Kapcsolat</h2>
             <p>Vedd fel velünk a kapcsolatot, írj üzenetet!</p>
-            <form action="/kapcsolat" method="post" class="formatted-form">
+            <form action="/kapcsolat" method="post" class="formatted-form" onsubmit="validate(event)">
                 <?php if (!$auth['authenticated']) {
                     print ' <div class="form-value">
                                 <div class="label"><label for="sender_name">Név</label></div>
-                                <div class="input"><input type="text" name="sender_name" id="sender_name" /></div>
+                                <div class="input col">
+                                    <input type="text" name="sender_name" id="sender_name" />
+                                    <p class="input-error" id="error_sender_name"></p>
+                                </div>
                             </div>';
                     print ' <div class="form-value">
                                 <div class="label"><label for="sender_email">E-mail cím</label></div>
-                                <div class="input"><input type="text" name="sender_email" id="sender_email" /></div>
+                                <div class="input col">
+                                    <input type="text" name="sender_email" id="sender_email" />
+                                    <p class="input-error" id="error_sender_email"></p>
+                                </div>
                             </div>';
                 }
                 else
                 {
                     print ' <div class="form-value">
                                 <div class="label"></div>
-                                <div class="input"><p>Bejelentkezett felhasználóként küldesz üzenetet.</p></div>
+                                <div class="input">
+                                    <p>Bejelentkezett felhasználóként küldesz üzenetet.</p></div>
                             </div>';
                 }
                 ?>
                 <div class="form-value">
                     <div class="label"><label for="message">Üzenet</label></div>
-                    <div class="input"><textarea name="message" id="message"></textarea></div>
+                    <div class="input col">
+                        <textarea name="message" id="message"></textarea>
+                        <p class="input-error" id="error_message"></p>
+                    </div>
                 </div>
                 <div class="form-value">
                     <div class="label"></div>
@@ -50,5 +60,7 @@
             </form>
         </div>
     </div>
+
+    <script src="/assets/validate.js"></script>
 
 <?php include 'Partials/layout.footer.php' ?>
